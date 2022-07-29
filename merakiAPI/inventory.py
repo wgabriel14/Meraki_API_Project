296277fa62b9ae9b4f06f625c259a2efcc2bacd0
wgabriel14 <reyesmanriquewg@gmail.com>
2022-07-29 14:35:25 -0400

    Se modifico el script inventory.py para que la consulta se ejecute cada 5 minutos y actualice el archivo CSV generado utilizando el modulo --time-- de python

--- merakiAPI/inventory.py
+++ merakiAPI/inventory.py
@@ -2,6 +2,7 @@ import requests
 import pprint as pp
 import json
 import csv
+import time
 
 def write_dicts_to_csv_fieldnames(list_of_dicts, file_path, field_names):
     with open(file_path, 'w', newline='', encoding='utf-8') as csv_file_object:
@@ -12,41 +13,43 @@ def write_dicts_to_csv_fieldnames(list_of_dicts, file_path, field_names):
 
 
 def inventory():
+    while(True):
+        #Api call
+        url = "https://api.meraki.com/api/v1/organizations/681155/devices/statuses?productTypes%5B%5D=appliance&productTypes%5B%5D=wireless"
 
-    #Api call
-    url = "https://api.meraki.com/api/v1/organizations/681155/devices/statuses?productTypes%5B%5D=appliance&productTypes%5B%5D=wireless"
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+        }
 
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
-    }
+        response = requests.request('GET', url, headers=headers)
 
-    response = requests.request('GET', url, headers=headers)
-
-    print("-----------------------------------------------------------------------")
-    print("API CALL: ")
-    print("-----------------------------------------------------------------------")
-    pp.pprint(response.json())
-    print("-----------------------------------------------------------------------")
-    print("Raise for status: ", response.raise_for_status())
-    print("-----------------------------------------------------------------------")    
-
-
+        print("-----------------------------------------------------------------------")
+        print("API CALL: ")
+        print("-----------------------------------------------------------------------")
+        pp.pprint(response.json())
+        print("-----------------------------------------------------------------------")
+        print("Raise for status: ", response.raise_for_status())
+        print("-----------------------------------------------------------------------")    
 
 
-    #keys to filter
-    jsontx = response.json()
-    list_keys = ['productType','model','name','mac','lanIp','publicIp','serial','status']
-    filter_dict = [{key:val for key, val in ele.items() if key in list_keys} for ele in jsontx]
-    print("-----------------------------------------------------------------------")
-    print("Filtered Data: ")
-    print("-----------------------------------------------------------------------")
-    pp.pprint(filter_dict)
 
 
-    path = 'inventory.csv'
-    write_dicts_to_csv_fieldnames(filter_dict, path, list_keys)
+        #keys to filter
+        jsontx = response.json()
+        list_keys = ['productType','model','name','mac','lanIp','publicIp','serial','status']
+        filter_dict = [{key:val for key, val in ele.items() if key in list_keys} for ele in jsontx]
+        print("-----------------------------------------------------------------------")
+        print("Filtered Data: ")
+        print("-----------------------------------------------------------------------")
+        pp.pprint(filter_dict)
+
+
+        path = 'inventory.csv'
+        write_dicts_to_csv_fieldnames(filter_dict, path, list_keys)
+
+        time.sleep(300)
 
 
 if __name__ == "__main__":
